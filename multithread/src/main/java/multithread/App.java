3746a7640784f919b0d53f60ba4e40a0e63d2f91
lijingwei <lijingwei@lvmama.com>
2017-06-29 14:41:37 +0800

    initial commit

---
+++ multithread/src/main/java/multithread/App.java
@@ -0,0 +1,13 @@
+package multithread;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
